18a1a3316bac968e05e837ec2ef0430c835f60fd
kazuya takama <42193989+taka1204@users.noreply.github.com>
2026-08-18 15:31:38 +0900

    Update title in hello.blade.php

--- laravel/resources/views/hello.blade.php
+++ laravel/resources/views/hello.blade.php
@@ -2,7 +2,7 @@
 <html lang="ja">
 <head>
   <meta charset="UTF-8">
-  <title>Hey, Kazuya!! Hello World</title>
+  <title>Hey, Kazuya!! Hello World!!</title>
 </head>
 <body>
   <h2>Hello World!!</h2>
